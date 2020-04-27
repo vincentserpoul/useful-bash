d95a5796f03b57b66081cedbd995e4e3d5f81e82
Vincent Serpoul <vincent@serpoul.com>
2020-04-27 11:49:16 +0800

    Update to be closer to POSIX

--- utils.sh
+++ utils.sh
@@ -2,29 +2,37 @@
 
 set -euo pipefail
 
+#==============================================================================#
+
 #============================  L o g g i n g  =================================#
 
 die() {
-	printf '\033[1;31mERROR:\033[0m %s\n' "$@" >&2  # bold red
-	exit 1
+    printf '\033[1;31mERROR:\033[0m %s\n' "$@" >&2 # bold red
+    exit 1
 }
 
 einfo() {
-	printf '\n\033[1;36m> %s\033[0m\n' "$@" >&2  # bold cyan
+    printf '\n\033[1;36m> %s\033[0m\n' "$@" >&2 # bold cyan
 }
 
 ewarn() {
-	printf '\033[1;33m> %s\033[0m\n' "$@" >&2  # bold yellow
+    printf '\033[1;33m> %s\033[0m\n' "$@" >&2 # bold yellow
 }
 
 usage() {
-	sed -En '/^#---help---/,/^#---help---/p' "$0" | sed -E 's/^# ?//; 1d;$d;'
+    sed -En '/^#---help---/,/^#---help---/p' "$0" | sed -E 's/^# ?//; 1d;$d;'
 }
 
-#=============================== D e p s ======================================#
+#================================ C h e c k ===================================#
 
-check_dep() {
+dep_check() {
     EXEC_CHECK=$1
-    hash "$EXEC_CHECK" 2>/dev/null || \
-	die "$EXEC_CHECK is required but it's not installed. Aborting."
+    hash "$EXEC_CHECK" 2>/dev/null ||
+        die "$EXEC_CHECK is required but it's not present. Aborting."
+}
+
+root_check() {
+    if [ "$(id -u)" -ne 0 ]; then
+        die 'This script must be run as root!'
+    fi
 }
