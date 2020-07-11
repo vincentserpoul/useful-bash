8e42229d9b21393931a21f7dab46c4bb60650f92
Vincent Serpoul <vincent@serpoul.com>
2020-07-11 12:00:13 +0800

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -23,16 +23,25 @@ usage() {
 	sed -En '/^#---help---/,/^#---help---/p' "$0" | sed -E 's/^# ?//; 1d;$d;'
 }
 
-#==============================  C h e c k s  =================================#
+#================================  C h e c k  =================================#
 
-dep_check() {
-	EXEC_CHECK=$1
+dep-check() {
+	local -r EXEC_CHECK=$1
+
 	hash "$EXEC_CHECK" 2>/dev/null ||
 		die "$EXEC_CHECK is required but it's not present. Aborting."
 }
 
-root_check() {
+root-check() {
 	if [ "$(id -u)" -ne 0 ]; then
 		die 'This script must be run as root!'
 	fi
+}
+
+#=================================  S o u r c e ===============================#
+
+source-folder() {
+	local -r SOURCE_FOLDER=$1
+
+	eval "$(find "$SOURCE_FOLDER" -maxdepth 1 -type f -exec echo . \'{}\'';' \;)"
 }
