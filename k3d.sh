1d88053d717f350d7d1ce059cbbe25453c2cbb56
Vincent Serpoul <vincent@serpoul.com>
2020-07-21 00:40:23 +0800

    update for k3d 3.0

--- k3d.sh
+++ k3d.sh
@@ -23,37 +23,26 @@ dep_check k3d
 
 k3s_cluster_create() {
     local -r CLUSTER_NAME=$1
-    local -r WORKERS_COUNT=${2:-3}
-    local -r PUBLISH_PORT=${3:-8080}
-    local -r API_PORT=${4:-6443}
-    local -r SERVER_ARG=${5:-""}
-    local -r AGENT_ARG=${6:-""}
+    local -r SERVER_COUNT=${2:-3}
+    local -r AGENT_COUNT=${3:-3}
+    local -r PUBLISH_PORT=${4:-8080}
+    local -r API_PORT=${5:-6443}
+    local -r SERVER_ARG=${6:-""}
+    local -r AGENT_ARG=${7:-""}
 
     if [[ -d "$CLUSTER_NAME" ]]; then die "you must define a cluster name"; fi
 
     einfo "creating k3d cluster ""$CLUSTER_NAME"" with ""$WORKERS_COUNT"" workers, listening on port ""$PUBLISH_PORT"", api on port ""$API_PORT"""
-    k3d create --name "$CLUSTER_NAME" \
-        --workers "$WORKERS_COUNT" \
+    k3d cluster create --name "$CLUSTER_NAME" \
+        --server "$SERVER_COUNT" \
+        --agent "$AGENT_COUNT" \
         --publish "$PUBLISH_PORT":80 \
         --api-port "$API_PORT" \
-        --server-arg "$SERVER_ARG" \
-        --agent-arg "$AGENT_ARG"
-
-    sleep 10s
+        --k3s-server-arg "$SERVER_ARG" \
+        --k3s-agent-arg "$AGENT_ARG" \
+        --wait
 
     k3s_cluster_kubeconfig_save "$CLUSTER_NAME"
-
-    k3s_cluster_wait_til_ready "$CLUSTER_NAME"
-}
-
-k3s_cluster_wait_til_ready() {
-    local -r CLUSTER_NAME=$1
-
-    einfo 'waiting for k3d cluster to be available'
-    sleep 5s
-    kubectl -n kube-system rollout status deployments/coredns
-    sleep 10s
-    kubectl -n kube-system rollout status deployments/traefik
 }
 
 k3s_cluster_context_file_path() {
@@ -67,7 +56,7 @@ k3s_cluster_delete() {
     local -r CONTEXT_FILE_PATH=$(k3s_cluster_context_file_path "$CLUSTER_NAME")
 
     ewarn 'deleting local k3d cluster'
-    k3d delete --name "$CLUSTER_NAME"
+    k3d cluster delete "$CLUSTER_NAME"
     kubecontext_delete "$CLUSTER_NAME"
     rm "$CONTEXT_FILE_PATH"
 }
@@ -78,7 +67,7 @@ k3s_cluster_kubeconfig_save() {
     local -r CONTEXT_FILE_PATH=$(k3s_cluster_context_file_path "$CLUSTER_NAME")
 
     mkdir -p "$HOME"/.kube
-    cat "$(k3d get-kubeconfig --name="$CLUSTER_NAME")" \
+    cat "$(k3d kubeconfig get "$CLUSTER_NAME")" \
         >"$CONTEXT_FILE_PATH"
 
     kubecontext_save "$CLUSTER_NAME" "$CONTEXT_FILE_PATH"
